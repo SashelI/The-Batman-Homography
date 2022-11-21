1439fd124284e35addb26b55d6f646af9becc1e6
Bordin Amelie <amelie.bordin@etudiant.univ-rennes1.fr>
2022-11-21 15:09:07 +0100

    Calibration : OK

--- TPVROB.cpp
+++ TPVROB.cpp
@@ -17,12 +17,35 @@ void onMouse(int action, int x, int y, int, void*) {
     }
 }
 
+string openCVType2str(int type) {
+    string r;
+
+    uchar depth = type & CV_MAT_DEPTH_MASK;
+    uchar chans = 1 + (type >> CV_CN_SHIFT);
+
+    switch (depth) {
+    case CV_8U:  r = "8U"; break;
+    case CV_8S:  r = "8S"; break;
+    case CV_16U: r = "16U"; break;
+    case CV_16S: r = "16S"; break;
+    case CV_32S: r = "32S"; break;
+    case CV_32F: r = "32F"; break;
+    case CV_64F: r = "64F"; break;
+    default:     r = "User"; break;
+    }
+
+    r += "C";
+    r += (chans + '0');
+
+    return r;
+}
+
 int main(int argc, char** argv)
 {
     string smallPath = R"(C:\Users\user\Documents\_ESIRTP\3\VROB\imtest.jpg)";
     string bigPath = R"(D:\Documents\_TPESIR\3\VROB_data\imtest.jpg)";
 
-    Mat I0full = imread(bigPath);
+    Mat I0full = imread(smallPath);
     Mat I0;
 
     resize(I0full, I0, Size(I0full.cols / 2, I0full.rows / 2)); //width, height
@@ -38,34 +61,49 @@ int main(int argc, char** argv)
             circle(I0, point, 2, Scalar{ 0, 0, 255 }, FILLED);
         }
         imshow("Corners Select", I0);
-        waitKey(25);
+        cv::waitKey(25);
     }
 
     //------------------------------------------------------------------------------
 
     imgPoint.push_back(X);
-    Mat test = Mat(3,1,CV_32F, 0.0f);
-    for (const auto& point : X)
-    {
-        cout << point.x << "," << point.y << endl;
-        X_3D.push_back(Point3f(point.x, point.y, 0.0f));
-        test.at<float>(0, 0) = point.x;
-        test.at<float>(1, 0) = point.y;
-        test.at<float>(1, 0) = 1.0f;
-    }
+
+    X_3D.push_back(Point3f(0.0f, 0.0f, 0.0f));
+    X_3D.push_back(Point3f(0.21f, 0.0f, 0.0f));
+    X_3D.push_back(Point3f(0.0f, 0.16f, 0.0f));
+    X_3D.push_back(Point3f(0.21f, 0.16f, 0.0f));
     objPoint.push_back(X_3D);
 
     calibrateCamera(objPoint, imgPoint, I0.size(), cameraMatrix, distCoeffs, R, T);
-    Mat Rmat;
-    Rodrigues(R[0], Rmat);
-    Mat RT;
-    hconcat(Rmat, T[0], RT);
-    RT.convertTo(RT, cameraMatrix.type());
-    test.convertTo(test, cameraMatrix.type());
-    Mat product = cameraMatrix * RT;
-    Mat Xmeter = product.inv() * test;
 
-    cout << Xmeter << endl;
+    vector<Mat> corners;
+    for (const auto& point : X)
+    {
+        Mat vectX = Mat(3, 1, CV_32F, 0.0f);
+        vectX.at<float>(0, 0) = point.x;
+        vectX.at<float>(1, 0) = point.y;
+        vectX.at<float>(2, 0) = 1.0f;
+        vectX.convertTo(vectX, cameraMatrix.type());
+        cout << vectX << endl;
+        corners.push_back(vectX);
+    }
+
+    vector<Mat>cornersMeter;
+    for (const auto& p : corners)
+    {
+        Mat Xmeter = cameraMatrix.inv() * p;
+        cout << Xmeter << endl;
+        cornersMeter.push_back(Xmeter);
+    }
+    for (const auto& p : cornersMeter) {
+        Mat px = cameraMatrix * p;
+        px.convertTo(px, CV_32SC1);
+        cout << px << endl;
+        Point point = Point{ px.at<int>(0,0), px.at<int>(1, 0)};
+        circle(I0, point, 5, Scalar{ 255, 0, 0 }, FILLED);
+    }
+    imshow("Corners", I0);
+    cv::waitKey(0);
 }
 
 
